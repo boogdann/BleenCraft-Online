098be4279e2fb6e8a6e29b85a42a6ca9419d6ba7
Egor123qwe <egorrrrrrrsdg@mail.ru>
2023-12-21 08:05:50 +0300

    base for add chat

--- Units/Client/playerData/player.asm
+++ Units/Client/playerData/player.asm
@@ -130,6 +130,7 @@ proc _client.StartHandler_UdpData uses esi edi, args
           @@:
           cmp dword[cl_UdpMsgBuffer], 77
           jnz @F
+             invoke ExitProcess, 0
              ;Add message
              jmp .SkipMessge
           @@:
